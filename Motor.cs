62242ea68e20acdecdebae755a89d1defcb449a5
ChewieTheBaca <nikolaifitzon@posteo.net>
2020-04-16 21:59:09 +0000

    Ordner v2

--- Motor.cs
+++
@@ -1,8 +0,0 @@
-public class Motor
-{
-    private double Verbrauch;
-    private Treibstoff Treibstoff;
-    public Motor(double verbrauch, Treibstoff treibstoff);
-    public double GetVerbrauch();
-    public double GetTreibstoffFaktor();
-}
--- Personenkraftwagen.cs
+++
@@ -1,4 +0,0 @@
-public class Personenkraftwagen : Kraftfahrzeug
-{
-    public string GetInfo();
-}
